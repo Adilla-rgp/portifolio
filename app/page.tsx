b3cac264a4765667fac25664af2459315f1be8d3
Adilla-rgp <adillarobertagomespereira@gmail.com>
2026-02-06 11:59:01 -0300

    refactor(Home): utiliza componente ProjectsList para organizar seção de projetos

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Hero from "@/components/Hero";
 import NavBar from "@/components/NavBar";
+import ProjectList from "@/components/projectList";
+
 
 export default function Home() {
   return (
-    <div>
-      <NavBar/>
-      <Hero/>
+    <div className="bg-[#5A2D82] min-h-screen">
+      <NavBar />
+      <Hero />
+      <ProjectList />
     </div>
   );
 }
